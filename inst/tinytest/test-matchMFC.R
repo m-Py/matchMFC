7be4484b0d29b98feacc6d2067f769396bba4d9b
Martin Papenberg <martin.papenberg@hhu.de>
2025-04-28 11:27:45 +0200

    refine tests

--- inst/tinytest/test-matchMFC.R
+++ inst/tinytest/test-matchMFC.R
@@ -1,16 +1,18 @@
 library(tinytest)
 library(matchMFC)
-
-### First test: without constraint on positive vs negative items
+library(anticlust)
 
 N <- 20
 distances <- as.matrix(dist(rnorm(N)))
 
 skew <- 2
 
-scales <- as.numeric(as.factor(sample(1:3, size = N, replace = TRUE)))
+n_scales <- 4
+stopifnot(N %% n_scales == 0) # much harder in general, if the distribution of scales is arbitrary
+scales <- sample(rep_len(1:n_scales, N))
 table(scales)
-#todo if cannot-link constraints can be fulfilled at all (anticlust:::optimal_cannot_link)
+
+positives <- sample(c(TRUE, FALSE), size = N, replace = TRUE)
 
 sum(!positives)
 size <- 2
@@ -22,31 +24,36 @@ uu <- matchMFC(
   time_limit = 5
 )
 table(uu)
-# verify that the three negatively poled items are in separate groups
-group_size_constraints_met <- function(x) {
+
+
+# Function to test if output has feasible structure regarding group sizes
+group_size_constraints_met <- function(x, size) {
   tab <- table(x)
-  all(tab == tab[1])
+  all(tab == size)
 }
 
-# group_size constraints are met
-expect_true(group_size_constraints_met(uu))
-expect_true(all(table(uu) == size))
+# Function to test if scales are all in different sets
+scales_in_different_sets <- function(x, scales) {
+  all(table(scales, x) <= 1)
+}
+
+table(uu)
+expect_true(group_size_constraints_met(uu, size))
 table(scales, uu)
-expect_true(all(table(scales, uu) <= 1)) #cannot be met, oftentimes!
+expect_true(scales_in_different_sets(uu, scales)) #cannot be met, oftentimes for arbitrary scale distribution
 
 
-## Now include positive vs. negative items
+## Another test case for triplets
 
-
-N <- 27
+N <- 21
 distances <- as.matrix(dist(rnorm(N)))
 
-skew <- 2
+n_scales <- 3
+stopifnot(N %% n_scales == 0) # much harder in general, if the distribution of scales is arbitrary
+scales <- sample(rep_len(1:n_scales, N))
+table(scales)
 
 positives <- sample(c(TRUE, FALSE), size = N, replace = TRUE)
-scales <- as.numeric(as.factor(sample(1:3, size = N, replace = TRUE)))
-table(scales)
-#todo if cannot-link constraints can be fulfilled at all (anticlust:::optimal_cannot_link)
 
 sum(!positives)
 size <- 3
@@ -55,19 +62,49 @@ uu <- matchMFC(
   distances, size = size,
   scale = scales,
   solver = "glpk",
-  time_limit = 5,
-  positive_polarity = positives
+  time_limit = 5
 )
-table(uu)
-# verify that the three negatively poled items are in separate groups
-group_size_constraints_met <- function(x) {
-  tab <- table(x)
-  all(tab == tab[1])
-}
 
 # group_size constraints are met
-expect_true(group_size_constraints_met(uu))
-expect_true(all(table(uu) == size))
-table(uu, positives)
+table(uu)
+expect_true(group_size_constraints_met(uu, size))
 table(scales, uu)
-all(table(scales, uu) <= 1) #cannot be met, oftentimes!
+expect_true(scales_in_different_sets(uu, scales)) #cannot be met, oftentimes for arbitrary scale distribution
+
+diversity_objective(distances, uu) #
+diversity_objective(distances, categorical_sampling(scales, n_groups)) # not as good as optimized!
+
+
+
+## Another test case for triplets
+
+N <- 21
+distances <- as.matrix(dist(rnorm(N)))
+
+n_scales <- 3
+stopifnot(N %% n_scales == 0) # much harder in general, if the distribution of scales is arbitrary
+scales <- sample(rep_len(1:n_scales, N))
+table(scales)
+
+positives <- sample(c(TRUE, FALSE), size = N, replace = TRUE)
+
+sum(!positives)
+size <- 3
+n_groups <- N/size
+uu <- matchMFC(
+  distances, size = size,
+  scale = scales,
+  solver = "glpk",
+  time_limit = 5
+)
+
+# group_size constraints are met
+table(uu)
+expect_true(group_size_constraints_met(uu, size))
+table(scales, uu)
+expect_true(scales_in_different_sets(uu, scales)) #cannot be met, oftentimes for arbitrary scale distribution
+
+diversity_objective(distances, uu) #
+diversity_objective(distances, categorical_sampling(scales, n_groups)) # not as good as optimized!
+
+
